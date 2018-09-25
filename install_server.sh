bfe139a5f6d5b98313ebd73e59f6ad33028e0ebf
xhafan <xhafan@gmail.com>
2018-09-25 11:55:41 +0200

    added awall firewall installation

--- install_server.sh
+++ install_server.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 set -x
 
+# install awall firewall 
+apk add ip6tables
+apk add -u awall
+modprobe ip_tables
+modprobe ip6_tables
+cp awallpolicy.json /etc/awall/optional
+awall enable awallpolicy
+awall activate -f
+rc-update add iptables
+rc-update add ip6tables
+rc-service iptables start
+rc-service ip6tables start
+
 # install and start docker
 echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
 apk update
