8191fdae8b9ad056bdf458fffcb99c364f163f32
xhafan <xhafan@gmail.com>
2018-09-26 11:56:21 +0200

    moved create docker network command down - didn't work immediately after start docker daemon command

--- install_server.sh
+++ install_server.sh
@@ -22,13 +22,15 @@ apk add docker
 rc-update add docker boot
 mkdir /etc/docker && cp daemon.json /etc/docker # disable docker messing up iptables which breaks hosts firewall rules
 service docker start
-docker network create --driver bridge --opt "com.docker.network.bridge.name"="br_docker" --subnet=172.16.0.0/16 xhafannet
 
 # install docker-compose
 apk add py-pip
 pip install --upgrade pip
 pip install docker-compose
 
+# create docker network for containers
+docker network create --driver bridge --opt "com.docker.network.bridge.name"="br_docker" --subnet=172.16.0.0/16 xhafannet
+               
 # configure ufw firewall so docker containers can communicate (https://svenv.nl/unixandlinux/dockerufw/)
 sed -i -e 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
 sed -i -e 's/*filter/*nat\n:POSTROUTING ACCEPT [0:0]\n-A POSTROUTING ! -o br_docker -s 172.16.0.0\/16 -j MASQUERADE\nCOMMIT\n*filter/g' /etc/ufw/before.rules
