6e777b6055510ea8927c5f22147a8124cc30d594
xhafan <xhafan@gmail.com>
2018-09-26 15:54:02 +0200

    fixing nginx logging hosts docker interface ip address instead of client http ip address - implementing ufw-docker solution from https://github.com/moby/moby/issues/4737#issuecomment-419705925

--- install_server.sh
+++ install_server.sh
@@ -14,24 +14,20 @@ ufw default deny incoming
 ufw default allow outgoing
 ufw limit SSH
 ufw allow in 80/tcp
-ufw --force enable
 rc-update add ufw
+
+# configure ufw firewall when used with docker (https://github.com/moby/moby/issues/4737#issuecomment-419705925)
+sed -i -e 's/# End required lines/:DOCKER-USER - [0:0]\n:ufw-user-input - [0:0]\n# End required lines/g' /etc/ufw/after.rules
+sed -i -e 's/# don'"'"'t delete the '"'"'COMMIT'"'"'/-A DOCKER-USER -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A DOCKER-USER -m conntrack --ctstate INVALID -j DROP\n-A DOCKER-USER -i eth0 -j ufw-user-input\n-A DOCKER-USER -i eth0 -j DROP\n\n# don'"'"'t delete the '"'"'COMMIT'"'"'/g' /etc/ufw/after.rules
+
+ufw --force enable
 
 # install and start docker
 apk add docker
 rc-update add docker boot
-mkdir /etc/docker && cp daemon.json /etc/docker # disable docker messing up iptables which breaks hosts firewall rules
 service docker start
 
 # install docker-compose
 apk add py-pip
 pip install --upgrade pip
 pip install docker-compose
-
-# create docker network for containers
-docker network create --driver bridge --opt "com.docker.network.bridge.name"="br_docker" --subnet=172.16.0.0/16 xhafannet
-               
-# configure ufw firewall so docker containers can communicate (https://svenv.nl/unixandlinux/dockerufw/)
-sed -i -e 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
-sed -i -e 's/*filter/*nat\n:POSTROUTING ACCEPT [0:0]\n-A POSTROUTING ! -o br_docker -s 172.16.0.0\/16 -j MASQUERADE\nCOMMIT\n*filter/g' /etc/ufw/before.rules
-ufw reload
