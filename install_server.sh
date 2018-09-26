7492ffe5f1634a72f7b5169d48aa2b1d79243272
xhafan <xhafan@gmail.com>
2018-09-26 11:21:53 +0200

    replaced awall firewall by ufw firewall (could not make docker containers reach internet with awall); docker now does not modify iptables

--- install_server.sh
+++ install_server.sh
@@ -3,30 +3,33 @@ set -x
 
 # update packages list
 echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
+echo "@testing http://nl.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 apk update
+
+# install ufw firewall 
+apk add ip6tables ufw@testing
+
+# configure firewall
+ufw default deny incoming
+ufw default allow outgoing
+ufw limit SSH
+ufw allow in 80/tcp
+ufw enable
+rc-update add ufw
+sed -i -e 's/few/asd/g' hello.txt                 
 
 # install and start docker
 apk add docker
+cp daemon.json /etc/docker # disable docker messing up iptables which breaks hosts firewall rules
 rc-update add docker boot
 service docker start
+docker network create --driver bridge --opt "com.docker.network.bridge.name"="br_docker" --subnet=172.16.0.0/16 xhafannet
 
 # install docker-compose
 apk add py-pip
 pip install --upgrade pip
 pip install docker-compose
 
-# install awall firewall 
-apk add ip6tables
-apk add -u awall
-modprobe ip_tables
-modprobe ip6_tables
-cp awallpolicy.json /etc/awall/optional
-awall enable awallpolicy
-awall activate -f
-rc-update add iptables
-rc-update add ip6tables
-rc-service iptables start
-rc-service ip6tables start
-
-# restart docker to pickup firewall changes
-service docker restart
+# configure ufw firewall so docker containers can communicate (https://svenv.nl/unixandlinux/dockerufw/)
+sed -i -e 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
+sed -i -e 's/*filter/*nat\n:POSTROUTING ACCEPT [0:0]\n-A POSTROUTING ! -o br_docker -s 172.16.0.0\/16 -j MASQUERADE\nCOMMIT\n*filter/g' /etc/ufw/before.rules
