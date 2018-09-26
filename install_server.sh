e1a76df264fe31943c6856b7f3ba42fe5d93f6c5
xhafan <xhafan@gmail.com>
2018-09-26 11:48:30 +0200

    fixed installation script after testing it

--- install_server.sh
+++ install_server.sh
@@ -14,14 +14,13 @@ ufw default deny incoming
 ufw default allow outgoing
 ufw limit SSH
 ufw allow in 80/tcp
-ufw enable
+ufw --force enable
 rc-update add ufw
-sed -i -e 's/few/asd/g' hello.txt                 
 
 # install and start docker
 apk add docker
-cp daemon.json /etc/docker # disable docker messing up iptables which breaks hosts firewall rules
 rc-update add docker boot
+mkdir /etc/docker && cp daemon.json /etc/docker # disable docker messing up iptables which breaks hosts firewall rules
 service docker start
 docker network create --driver bridge --opt "com.docker.network.bridge.name"="br_docker" --subnet=172.16.0.0/16 xhafannet
 
@@ -33,3 +32,4 @@ pip install docker-compose
 # configure ufw firewall so docker containers can communicate (https://svenv.nl/unixandlinux/dockerufw/)
 sed -i -e 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
 sed -i -e 's/*filter/*nat\n:POSTROUTING ACCEPT [0:0]\n-A POSTROUTING ! -o br_docker -s 172.16.0.0\/16 -j MASQUERADE\nCOMMIT\n*filter/g' /etc/ufw/before.rules
+ufw reload
